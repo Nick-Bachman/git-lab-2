c1f228822e39c99a83a117c995827978938b1353
Nick Bachman <nb820617@ohio.edu>
2019-09-06 15:22:09 -0400

    rfgtuyhtyuj

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nick Bachman>
+ *        Date: <9/6/2019>
+ * Description: Outputing text to the screen
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
